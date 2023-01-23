c30defbbb2a86a51f3584a39e2f86ecbb9305559
mukherjeesudebi <sudebi@vaadin.com>
2023-01-23 15:27:57 +0530

    added project drop down to header
    added project drop down to header

--- src/main/java/com/vaadin/bugrap/views/MainLayout.java
+++ src/main/java/com/vaadin/bugrap/views/MainLayout.java
@@ -1,21 +1,29 @@
 package com.vaadin.bugrap.views;
 
 import com.vaadin.bugrap.dao.ProjectDao;
-import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.router.Route;
 
 @Route("")
 public class MainLayout extends VerticalLayout {
 
-	private H2 viewTitle;
 	private ProjectDao projectDao;
 	
 	public MainLayout(ProjectDao projectDao) {
-		this.projectDao = projectDao;
-		viewTitle = new H2("Hello Sudebi");
-		add(viewTitle);
+		this.projectDao = projectDao;		
+		addHeader();
+	}
+	
+	public void addHeader() {
+		HorizontalLayout horizontalLayout = new HorizontalLayout();
 		
-		projectDao.getAllProjectNames().stream().forEach(p-> System.out.println(p));
+		Select<String> select = new Select<>();
+		select.setItems(projectDao.getAllProjectNames());
+		select.setValue(projectDao.getAllProjectNames().get(0));
+		horizontalLayout.add(select);
+		
+		add(horizontalLayout);
 	}
 }
