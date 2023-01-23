14ce0e12e66bd4891cf658b6b3913a62f606b8ca
mukherjeesudebi <sudebi@vaadin.com>
2023-01-23 17:22:20 +0530

    function buttons added
    function buttons added with horizontal layout in a vertical layout body

--- src/main/java/com/vaadin/bugrap/views/MainLayout.java
+++ src/main/java/com/vaadin/bugrap/views/MainLayout.java
@@ -1,7 +1,8 @@
 package com.vaadin.bugrap.views;
 
 import com.vaadin.bugrap.dao.ProjectDao;
-import com.vaadin.flow.component.html.H6;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -17,6 +18,7 @@ public class MainLayout extends VerticalLayout {
 	public MainLayout(ProjectDao projectDao) {
 		this.projectDao = projectDao;		
 		addHeader();
+		addBody();
 	}
 	
 	public void addHeader() {
@@ -29,13 +31,39 @@ public class MainLayout extends VerticalLayout {
 		
 		HorizontalLayout headerHorizontalLayoutRight = new HorizontalLayout();
 		Icon userIcon = new Icon(VaadinIcon.USER);
-		H6 userName = new H6("Marc Manager");
+		Div userName = new Div();
+		userName.setText("Marc Manager");
 		Icon powerOffIcon = new Icon(VaadinIcon.POWER_OFF);
 		headerHorizontalLayoutRight.add(userIcon,userName,powerOffIcon);
+		headerHorizontalLayoutRight.getStyle().set("color", "#3c86ce");
 		headerHorizontalLayout.add(headerHorizontalLayoutRight);
 		
 		headerHorizontalLayout.setWidthFull();
-		headerHorizontalLayout.setAlignItems(Alignment.STRETCH);
+		headerHorizontalLayout.setAlignItems(Alignment.CENTER);
+		headerHorizontalLayout.setJustifyContentMode(JustifyContentMode.BETWEEN);
+		headerHorizontalLayout.getStyle().set("box-shadow", "0 4px 7px -2px gray");
 		add(headerHorizontalLayout);
+		setHeightFull();
+	}
+	
+	public void addBody() {
+		VerticalLayout verticalBodyLayout = new VerticalLayout();
+		verticalBodyLayout.setHeightFull();
+		verticalBodyLayout.setWidthFull();
+		verticalBodyLayout.getStyle().set("background-color", "#e8e5e5");
+		addFunctionButtons(verticalBodyLayout);
+		add(verticalBodyLayout);
+	}
+	
+	public void addFunctionButtons(VerticalLayout verticalBodyLayout) {
+		HorizontalLayout functionButtonsLayout = new HorizontalLayout();
+		Button bugButton = new Button("Report a bug",new Icon(VaadinIcon.BUG)); 
+		Button failureRequestButton = new Button("Request a failure",new Icon(VaadinIcon.LIGHTBULB)); 
+		Button manageProjectButton = new Button("Manage Project",new Icon(VaadinIcon.SUN_O)); 
+		
+		functionButtonsLayout.add(bugButton,failureRequestButton,manageProjectButton);
+		verticalBodyLayout.add(functionButtonsLayout);
+		
+		
 	}
 }
