5cbb8917682ddba3c76b5419b92c83d7f8d09eb9
mukherjeesudebi <sudebi@vaadin.com>
2023-02-08 20:56:43 +0530

    added some more checks

--- src/test/java/com/vaadin/bugrap/integrationtest/DistributionBarLayoutIT.java
+++ src/test/java/com/vaadin/bugrap/integrationtest/DistributionBarLayoutIT.java
@@ -11,6 +11,8 @@ import com.vaadin.flow.component.textfield.testbench.TextFieldElement;
 
 public class DistributionBarLayoutIT extends DistributionBarLayoutTest {
 
+    DistributionBarElement distributionBarElement;
+
     @Override
     public void setup() throws Exception {
         super.setup();
@@ -26,35 +28,71 @@ public class DistributionBarLayoutIT extends DistributionBarLayoutTest {
 
     @Test
     public void test() throws InterruptedException {
-        DistributionBarElement distributionBarElement = $(
-                DistributionBarElement.class).first();
+        distributionBarElement = $(DistributionBarElement.class).first();
 
         $(TextFieldElement.class).id("closedCount").setValue("10");
         $(TextFieldElement.class).id("unAssignedCount").setValue("50");
         $(TextFieldElement.class).id("unResolvedCount").setValue("5");
 
-        Thread.sleep(50);
+        Thread.sleep(500);
         $(ButtonElement.class).id("updateCountsButtons").click();
-        Thread.sleep(2000);
 
-        assertEquals(10, ((Long)distributionBarElement.getProperty("closed")).intValue());
-        assertEquals(50, ((Long)distributionBarElement.getProperty("unAssigned")).intValue());
-        assertEquals(5, ((Long)distributionBarElement.getProperty("unResolved")).intValue());
-        
-        
+        Thread.sleep(1000);
+
+        assertEquals(10, ((Long) distributionBarElement.getProperty("closed"))
+                .intValue());
+        assertEquals(50,
+                ((Long) distributionBarElement.getProperty("unAssigned"))
+                        .intValue());
+        assertEquals(5,
+                ((Long) distributionBarElement.getProperty("unResolved"))
+                        .intValue());
+
+        checkWidths(10, 50, 5);
+
         $(TextFieldElement.class).id("closedCount").setValue("3");
-        Thread.sleep(1000);
         $(TextFieldElement.class).id("unAssignedCount").setValue("8");
-        Thread.sleep(1000);
         $(TextFieldElement.class).id("unResolvedCount").setValue("1");
-        Thread.sleep(1000);
-        $(ButtonElement.class).id("updateCountsButtons").click();
-        Thread.sleep(2000);
 
-        assertEquals(3, ((Long)distributionBarElement.getProperty("closed")).intValue());
-        assertEquals(8, ((Long)distributionBarElement.getProperty("unAssigned")).intValue());
-        assertEquals(1, ((Long)distributionBarElement.getProperty("unResolved")).intValue());
+        Thread.sleep(500);
+        $(ButtonElement.class).id("updateCountsButtons").click();
+        Thread.sleep(1000);
+
+        assertEquals(3, ((Long) distributionBarElement.getProperty("closed"))
+                .intValue());
+        assertEquals(8,
+                ((Long) distributionBarElement.getProperty("unAssigned"))
+                        .intValue());
+        assertEquals(1,
+                ((Long) distributionBarElement.getProperty("unResolved"))
+                        .intValue());
         
-        
+        checkWidths(3,8,1);
+
+    }
+
+    public void checkWidths(int closed, int unassigned, int unresolved) {
+
+        int totalCount = closed + unassigned + unresolved;
+        if (totalCount >= 13) {
+            assertEquals(Math.round(((float) closed / totalCount) * 100) + "%",
+                    distributionBarElement.getProperty("closedWidth"));
+            assertEquals(
+                    Math.round(((float) unassigned / totalCount) * 100) + "%",
+                    distributionBarElement.getProperty("unAssignedWidth"));
+            assertEquals(
+                    Math.round(((float) unresolved / totalCount) * 100) + "%",
+                    distributionBarElement.getProperty("unResolvedWidth"));
+
+        } else {
+
+            assertEquals(closed * 30 + "px",
+                    distributionBarElement.getProperty("closedWidth"));
+            assertEquals(unassigned * 30 + "px",
+                    distributionBarElement.getProperty("unAssignedWidth"));
+            assertEquals(unresolved * 30 + "px",
+                    distributionBarElement.getProperty("unResolvedWidth"));
+        }
+
     }
 }
